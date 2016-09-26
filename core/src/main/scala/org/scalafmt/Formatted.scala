e8eff04f03be024771175cea0d3de65ce62b514d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-09-26 19:28:27 +0200

    Add toEither method on Formatted

--- core/src/main/scala/org/scalafmt/Formatted.scala
+++ core/src/main/scala/org/scalafmt/Formatted.scala
@@ -1,6 +1,12 @@
 package org.scalafmt
 
 sealed abstract class Formatted {
+
+  def toEither: Either[Throwable, String] = this match {
+    case Formatted.Success(s) => Right(s)
+    case Formatted.Failure(e) => Left(e)
+  }
+  
   def get: String = this match {
     case Formatted.Success(code) => code
     case Formatted.Failure(e) => throw e
